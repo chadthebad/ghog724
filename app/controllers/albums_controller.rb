6a9cd3e165ef1fb344e35ff899b4d5f54e0387a4
Chad Dibble <chad@chadthebad.com>
2012-07-24 12:52:51 -0700

    simple controller for albums

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,3 +1,37 @@
-
 class AlbumsController < ApplicationController
+
+  def index
+    @albums = Album.all
+  end
+  
+  def new
+    @album = Album.new
+  end
+  
+  def create
+    @album = Album.new(params[:album])
+    @album.save
+    redirect_to @album
+  end
+  
+  def show
+    @album = Album.find(params[:id])
+  end
+  
+  def edit
+    @album = Album.find(params[:id])
+  end
+  
+  def update
+    @album = Album.find(params[:id])
+    @album.update_attributes(params[:album])
+    redirect_to @album
+  end
+  
+  def destroy
+    @album = Album.find(params[:id])
+    @album.destroy
+    redirect_to '/'
+  end
+
 end
--- app/models/album.rb
+++ app/models/album.rb
@@ -1,3 +1,8 @@
 class Album < ActiveRecord::Base
+
+  has_many :photos
+  belongs_to :user
+
   attr_accessible :title, :user_id
+
 end
